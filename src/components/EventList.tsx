4dc39a6ecb474e0b8cb46dba247c5f73958a2671
Xiaoru Sun <xiaorusun@Xiaorus-MacBook-Air.local>
2024-12-17 20:43:39 +0000

    adds EventCard component

--- src/components/EventList.tsx
+++ src/components/EventList.tsx
@@ -1,38 +1,11 @@
 import events from '../assets/events.json';
+import { EventCard } from './EventCard';
 
-type Props = {
-  event: {
-    id: string;
-    title: string;
-    description: string;
-    datetime: string;
-    location: string;
-    image: string;
-    status: string;
-    interestedCount: number;
-  };
-};
 const EventList = () => {
   return (
     <>
       {events.map((event, index) => {
-        return (
-          <div className="p-3 w-full">
-            <div key={index} className="flex flex-row items-start">
-              <div className="flex-1">
-                <p>{event.datetime}</p>
-                <p>{event.title}</p>
-                <p>{event.location}</p>
-              </div>
-
-              <img
-                src={event.image}
-                alt="event image"
-                className="ml-2 w-[36%] aspect-video hover:aspect-square"
-              ></img>
-            </div>
-          </div>
-        );
+        return <EventCard event={event} index={index} />;
       })}
     </>
   );
