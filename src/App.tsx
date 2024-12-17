7bba15984f5671b5599cec6b18545358a98e363f
Xiaoru Sun <xiaorusun@Xiaorus-MacBook-Air.local>
2024-12-17 17:30:36 +0000

    adds events data, styling on event item

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
-import { Route, Routes } from 'react-router-dom';
+import { Link, Route, Routes } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
-import Events from './components/Events';
+import EventList from './components/EventList';
 
 function App() {
   return (
     <div>
       <header>I am header</header>
+      {/* <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/events">Event page</Link>
+        </li>
+      </ul> */}
       <Routes>
         <Route path="/" element={<LandingPage />}></Route>
-        <Route path="/events" element={<Events />}></Route>
+        <Route path="/events" element={<EventList />}></Route>
       </Routes>
     </div>
   );
