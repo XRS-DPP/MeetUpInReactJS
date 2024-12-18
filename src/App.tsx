62fbcfe37c3db48f55428660b058dcd923c57d8f
Xiaoru Sun <xiaorusun@Xiaorus-MacBook-Air.local>
2024-12-18 22:50:30 +0000

    imports header to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import { Link, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
 import EventList from './components/EventList';
 import EventPage from './components/EventPage';
+import Header from './components/Header';
 
 function App() {
   return (
     <div>
+      <Header></Header>
       <Routes>
         <Route path="/" element={<LandingPage />}></Route>
         <Route path="/events" element={<EventList />}></Route>
