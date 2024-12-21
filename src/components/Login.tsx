0320c367ae3d46e348a8667cd8977e178af5d2c5
Xiaoru Sun <xiaorusun@Xiaorus-MacBook-Air.local>
2024-12-21 21:43:33 +0000

    adds Login component with form of log in

---
+++ src/components/Login.tsx
@@ -0,0 +1,57 @@
+import { HtmlHTMLAttributes, useState } from 'react';
+
+export const Login = () => {
+  const admin = { email: 'admin@meetupnow.com', password: 'IamSuperAdmin!' };
+
+  const [input, setInput] = useState({ email: '', password: '' });
+  const [isLoggedin, setIsLoggedin] = useState<Boolean>(false);
+
+  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setInput((pre) => ({ ...pre, [name]: value }));
+  };
+
+  const handleLogin = () => {
+    if (input.email === admin.email && input.password === admin.password) {
+      console.log('logged in');
+      setIsLoggedin(true);
+    }
+  };
+
+  return (
+    <div className="p-2">
+      <p className="text-m font-semibold pl-2 mt-4 text-primary">Log in</p>
+      <form
+        className="flex flex-col gap-5 p-2 mt-4"
+        onSubmit={(e) => {
+          e.preventDefault;
+          handleLogin();
+        }}
+      >
+        <input
+          type="email"
+          placeholder="Email"
+          value={input.email}
+          name="email"
+          onChange={handleInput}
+          className="border-2 rounded-md p-3 "
+          required={true}
+        ></input>
+
+        <input
+          type="password"
+          placeholder="Password"
+          value={input.password}
+          className="border-2 rounded-md p-3"
+          name="password"
+          onChange={handleInput}
+          required={true}
+          autoComplete="on"
+        ></input>
+        <button className="bg-secodary text-white font-semibold p-3 rounded-md text-center text-s mt-4">
+          Log In
+        </button>
+      </form>
+    </div>
+  );
+};
