606cafc6060b7a62e545a70a61d4498542a11490
Xiaoru Sun <xiaorusun@Xiaorus-MacBook-Air.local>
2024-12-22 23:15:44 +0000

    refactors EventList, adds eventList state at app level

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,25 @@ import EventList from './components/EventList';
 import EventPage from './components/EventPage';
 import Header from './components/Header';
 import { Login } from './components/Login';
+import events from './assets/events.json';
+import { useState } from 'react';
 
 function App() {
+  const STORE_KEY = 'EVENTS_STORE';
+  const storedEvents = JSON.parse(localStorage.getItem(STORE_KEY) || '[]');
+  const [eventList, setEventList] = useState(
+    storedEvents.length ? storedEvents : events,
+  );
+
   return (
     <div>
       <Header></Header>
       <Routes>
         <Route path="/" element={<LandingPage />}></Route>
-        <Route path="/events" element={<EventList />}></Route>
+        <Route
+          path="/events"
+          element={<EventList eventList={eventList} />}
+        ></Route>
         <Route path="/events/:id" element={<EventPage />}></Route>
         <Route path="/login" element={<Login />}></Route>
       </Routes>
