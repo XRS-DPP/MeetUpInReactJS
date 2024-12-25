e6cbb52a0ffb32cdccc44fb3e112f853066479ff
Xiaoru Sun <xiaorusun@Xiaorus-MacBook-Air.local>
2024-12-25 18:20:52 +0000

    reuse EventForm for CreateEvent

--- src/pages/CreateEvent.tsx
+++ src/pages/CreateEvent.tsx
@@ -1,4 +1,5 @@
-import { Dispatch, SetStateAction, useState } from 'react';
+import { SetStateAction, useState } from 'react';
+import EventForm from '../components/EventForm';
 
 type Event = {
   id: number;
@@ -13,7 +14,7 @@ type Event = {
 
 type Props = {
   eventList: Event[];
-  setEventList: Dispatch<SetStateAction<Event[]>>;
+  setEventList: React.Dispatch<SetStateAction<Event[]>>;
 };
 
 const CreateEvent = ({ eventList, setEventList }: Props) => {
@@ -29,7 +30,7 @@ const CreateEvent = ({ eventList, setEventList }: Props) => {
     interestedCount: 0,
   });
 
-  const handleEventInput = (
+  const handleEventInputChange = (
     e:
       | React.ChangeEvent<HTMLInputElement>
       | React.ChangeEvent<HTMLTextAreaElement>,
@@ -39,83 +40,35 @@ const CreateEvent = ({ eventList, setEventList }: Props) => {
     SetEventInput((prev) => ({ ...prev, [name]: value }));
   };
 
-  const handleSubmitEvent = () => {
+  const handleCreateEvent = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
     const maxId = eventList.reduce((max, obj) => {
       return +obj.id > max ? obj.id : max;
     }, 0);
+
     setEventList((prev) => [...prev, { ...eventInput, id: maxId * 1 + 1 }]);
+    setEventAdded(true);
+    SetEventInput({
+      id: 0,
+      title: '',
+      description: '',
+      datetime: '',
+      location: '',
+      image: '',
+      status: 'live',
+      interestedCount: 0,
+    });
   };
 
   return (
-    <section className="text-center mt-6 p-3 ">
+    <section>
       <h2 className="text-secodary font-semibold text-s">Add a new event</h2>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          handleSubmitEvent();
-          setEventAdded(true);
-          SetEventInput({
-            id: 0,
-            title: '',
-            description: '',
-            datetime: '',
-            location: '',
-            image: '',
-            status: 'live',
-            interestedCount: 0,
-          });
-        }}
-        className="flex flex-col gap-2 p-2 mt-3"
-      >
-        <input
-          placeholder="Title"
-          type="text"
-          name="title"
-          value={eventInput.title}
-          onChange={handleEventInput}
-          className="p-2 border-2 rounded-md"
-          required
-        ></input>
-        <textarea
-          placeholder="Description"
-          name="description"
-          value={eventInput.description}
-          onChange={handleEventInput}
-          className="p-2 border-2 rounded-md"
-          required
-        ></textarea>
-
-        <input
-          placeholder="YYYY-MM-DDTHH:00:00"
-          type="datetime-local"
-          name="datetime"
-          value={eventInput.datetime}
-          onChange={handleEventInput}
-          className="p-2 border-2 rounded-md"
-          required
-        ></input>
-
-        <input
-          placeholder="Location"
-          type="text"
-          name="location"
-          value={eventInput.location}
-          onChange={handleEventInput}
-          className="p-2 border-2 rounded-md"
-          required
-        ></input>
-        <input
-          placeholder="Image URL"
-          name="image"
-          value={eventInput.image}
-          onChange={handleEventInput}
-          className="p-2 border-2 rounded-md"
-          required
-        ></input>
-        <button className="bg-secodary text-white p-3 rounded-md mt-5 cursor-pointer">
-          CREATE
-        </button>
-      </form>
+      <EventForm
+        eventInput={eventInput}
+        handleSubmitForm={handleCreateEvent}
+        name="Create"
+        handleEventInputChange={handleEventInputChange}
+      />
       {eventAdded && <p>Event added</p>}
     </section>
   );
