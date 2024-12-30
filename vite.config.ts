76c566c927b5f851889f09088ae63af5ef41a648
Xiaoru Sun <xiaorusun@Xiaorus-MacBook-Air.local>
2024-12-30 10:53:46 +0000

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: '/MeetUpInReactJS/',
-  base: '/',
+  base: '/MeetUpInReactJS/',
+  // base: '/',
   build: {
     outDir: 'dist',
   },
