65e2c9a513fde9160217ca47622285a41ac84bcf
Xiaoru Sun <xiaorusun@Xiaorus-MacBook-Air.local>
2025-01-17 21:59:11 +0000

    replaces browser router with HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,15 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { AuthProvider } from './contexts/Auth.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename="/MeetUpInReactJS">
+    <HashRouter basename="/MeetUpInReactJS">
       <AuthProvider>
         <App />
       </AuthProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 );
