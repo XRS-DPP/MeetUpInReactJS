493fb670b5ea028d981e3af2f5caf5ab310dce67
Xiaoru Sun <xiaorusun@Xiaorus-MacBook-Air.local>
2025-01-17 22:21:23 +0000

    HashRouter without basename

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import { AuthProvider } from './contexts/Auth.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <HashRouter basename="/MeetUpInReactJS">
+    <HashRouter>
       <AuthProvider>
         <App />
       </AuthProvider>
